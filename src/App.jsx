6ab61f8eae73ba26b9bb4cad278a786b2e2e1b7c
Sahil-Arsalan <arsalanalam152@gmail.com>
2026-09-13 17:20:28 +0530

    Fix component import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Skills from './components/skills'
-import Projects from './components/projects'
-import Experience from './components/Experience'
-import Contact from './components/contact'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
